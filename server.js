8195dc5afea6d85eef93f7a946f6cd1074a5024e
Donna Kuang <dolaku@gmail.com>
2019-05-22 16:29:26 -0400

    Working on routes

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const express = require('express');
+const path = require('path');
 
+// From the express Constructor above
+// New Express() is being called behind the scenes
 const app = express();
 
+// Either use Heroku deployment OR localhost
 const PORT = process.env.PORT || 8080;
 
+// Express gives the browser access to anything in the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+// Sets up Express to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// ROUTES ============
+require('./app/routing/htmlRoutes.js')(app);
+require('./app/routing/apiRoutes.js')(app);
+
 
 // LISTENER ==========
-app.listen(PORT, () => {
-    console.log(`App listening on PORT: ${PORT}`);
-})
+app.listen(PORT, () => console.log(`App listening on PORT: ${PORT}`));
