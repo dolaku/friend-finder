6a0012a159d8379ddc6bf66628a06fad5dd3f3a6
Donna Kuang <dolaku@gmail.com>
2019-05-23 14:20:50 -0400

    Adjusted routes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,10 +2,10 @@ const path = require('path');
 
 module.exports = (app) => {
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../public/home.html'));
-    });
     app.get('/survey', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/survey.html'));
+    });
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/home.html'));
     });
 }
