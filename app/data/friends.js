f62100ef01e62ddcb08b2cd49ad572cb75cf8d39
dolaku <dolaku@gmail.com>
2019-06-01 21:31:18 -0400

    Created friends data

---
+++ app/data/friends.js
@@ -0,0 +1,84 @@
+var friendList = [
+    {
+       name: "Tom",
+       photo: "https://images.unsplash.com/photo-1528892952291-009c663ce843?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+       scores: [
+          2,
+          1,
+          5,
+          4,
+          3,
+          2,
+          1,
+          5,
+          4,
+          3
+       ]
+    },
+    {
+       name: "Gary",
+       photo: "https://images.unsplash.com/photo-1502980426475-b83966705988?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+       scores: [
+          2,
+          4,
+          1,
+          3,
+          5,
+          2,
+          4,
+          1,
+          3,
+          5
+       ]
+    },
+    {
+       name: "Taylor",
+       photo: "https://images.unsplash.com/photo-1505840717430-882ce147ef2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+       scores: [
+          1,
+          4,
+          5,
+          3,
+          2,
+          2,
+          2,
+          4,
+          5,
+          2
+       ]
+    },
+    {
+       name: "Sarah",
+       photo: "https://images.pexels.com/photos/235534/pexels-photo-235534.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+       scores: [
+          1,
+          2,
+          3,
+          4,
+          5,
+          4,
+          3,
+          2,
+          1,
+          5
+       ]
+    },
+    {
+       name: "Frank",
+       photo: "https://images.unsplash.com/photo-1545704881-d5dfa19efa38?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+       scores: [
+          3,
+          4,
+          4,
+          5,
+          5,
+          4,
+          5,
+          3,
+          4,
+          5
+       ]
+    }
+ ];
+ 
+ module.exports = friendList;
