bcbddb0aef7658d5f61ef62695d61567afb28ffd
dolaku <dolaku@gmail.com>
2019-06-02 21:39:31 -0400

    Gets user input on survey submit

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,31 +3,31 @@ var friendList = [
        name: "Tom",
        photo: "https://images.unsplash.com/photo-1528892952291-009c663ce843?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
        scores: [
-          2,
-          1,
-          5,
-          4,
-          3,
-          2,
-          1,
-          5,
-          4,
-          3
+          32,
+          81,
+          25,
+          84,
+          93,
+          42,
+          51,
+          15,
+          34,
+          73
        ]
     },
     {
        name: "Gary",
        photo: "https://images.unsplash.com/photo-1502980426475-b83966705988?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
        scores: [
-          2,
-          4,
-          1,
-          3,
-          5,
-          2,
-          4,
-          1,
-          3,
+          12,
+          24,
+          31,
+          43,
+          55,
+          62,
+          74,
+          81,
+          93,
           5
        ]
     },
@@ -35,48 +35,48 @@ var friendList = [
        name: "Taylor",
        photo: "https://images.unsplash.com/photo-1505840717430-882ce147ef2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
        scores: [
-          1,
-          4,
+          81,
+          74,
+          65,
+          53,
+          42,
+          32,
+          22,
+          14,
           5,
-          3,
-          2,
-          2,
-          2,
-          4,
-          5,
-          2
+          92
        ]
     },
     {
        name: "Sarah",
        photo: "https://images.pexels.com/photos/235534/pexels-photo-235534.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
        scores: [
-          1,
-          2,
-          3,
-          4,
-          5,
-          4,
-          3,
-          2,
-          1,
-          5
+          21,
+          42,
+          73,
+          94,
+          15,
+          44,
+          83,
+          92,
+          31,
+          75
        ]
     },
     {
        name: "Frank",
        photo: "https://images.unsplash.com/photo-1545704881-d5dfa19efa38?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
        scores: [
-          3,
-          4,
-          4,
-          5,
-          5,
-          4,
-          5,
-          3,
-          4,
-          5
+          23,
+          84,
+          24,
+          75,
+          15,
+          84,
+          35,
+          63,
+          44,
+          95
        ]
     }
  ];
